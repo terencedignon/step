7cdf571b06a30c513d77a51d5c667352f47f0e2b
Terence Dignon <terencedignon@google.com>
2020-06-30 00:44:18 -0400

    mess up js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var HELLO = "something";
+
 function expandImage(imgs) {
   var expandImg = document.getElementById("expandedImg");
   expandImg.src = imgs.src;
